032715cfc3cd9917397ac42a42e08c00ed976a94
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-12-23 18:55:48 +0100

    create the header :arrow_right: index.php && style.css

--- index.php
+++ index.php
@@ -7,6 +7,17 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <header>
+        <h1>Flashcards</h1>
+
+        <ul>
+        <li>Categories :</li>
+        <a href="#"><li>link1</li></a>
+        <a href="#"><li>link2</li></a>
+        <a href="#"><li>link3</li></a>
+        <a href="#"><li>link4</li></a>
+        <a href="#"><li>create flashCards</li></a>
+        </ul>
+    </header>
 </body>
 </html>
