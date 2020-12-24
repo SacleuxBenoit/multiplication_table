820d216074afea57bdc99ef162eab45442ba365b
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-12-24 12:53:46 +0100

    add form with 3 inputs : 2 type text, 1 type submit :arrow_right: createCards.php... and create the file createCards.css in the folder css

--- createCards.php
+++ createCards.php
@@ -3,12 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/createCards.css">
     <title>Document</title>
 </head>
 <body>
     <?php
         include('components/header.php')
     ?>
+<div class="divForm"> 
+    <form action="#" method="post">
+
+        
+            <p>
+                <label for="front">Front :</label>
+                <input type="text" name="front" id="front">
+            </p>
+
+            <p>
+                <label for="back">Back :</label>
+                <input type="text" name="back" id="back">
+            </p>
+
+            <p>
+                <input type="submit" value="submit">
+            </p>
+    </form>
+    </div>
+
 </body>
 </html>
