16dd8781f929bebdb43fcfbabdcc7069123a9b34
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-12-25 20:04:47 +0100

    add verification for see if $_POST['front'] && $_POST['back'] is empty or not :arrow_right: addCards.php

--- database/addCards.php
+++ database/addCards.php
@@ -2,8 +2,14 @@
 session_start();
 include('connection_database.php');
 
-$get_cards = $bdd->prepare('INSERT INTO addcards(front,back) VALUES(:front,:back)');
-$get_cards->bindParam(':front', $_POST['front']);
-$get_cards->bindParam(':back', $_POST['back']);
-$get_cards->execute();
+if(!empty($_POST['front']) && !empty($_POST['back'])){
+    $get_cards = $bdd->prepare('INSERT INTO addcards(front,back) VALUES(:front,:back)');
+    $get_cards->bindParam(':front', $_POST['front']);
+    $get_cards->bindParam(':back', $_POST['back']);
+    $get_cards->execute();
+    header('Location: ../index.php');
+}else{
+    header('Location: ../index.php');
+}
+
 ?>
