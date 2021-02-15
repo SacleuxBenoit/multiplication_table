028473e9456be84a2dcf38e833d690499682c364
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-02-15 18:33:29 +0100

    add link to style.css :arrow_right: deleteCards.php and change the Location in the header :arrow_right:  addCards.php

--- database/addCards.php
+++ database/addCards.php
@@ -7,9 +7,9 @@ if(!empty($_POST['front']) && !empty($_POST['back'])){
     $get_cards->bindParam(':front', $_POST['front']);
     $get_cards->bindParam(':back', $_POST['back']);
     $get_cards->execute();
-    header('Location: ../createCards.php');
+    header('Location: http://localhost/flashcards/createCards.php');
 }else{
-    header('Location: ../createCards.php');
+    header('Location: http://localhost/flashcards/createCards.php');
 }
 
 ?>
