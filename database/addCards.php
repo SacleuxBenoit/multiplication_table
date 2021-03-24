7cb8e46fb3821458c517de544f21d5d3a5437a34
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-24 16:33:54 +0100

    :memo: correctif : change the header Location :arrow_right: addCards.php

--- database/addCards.php
+++ database/addCards.php
@@ -7,9 +7,9 @@ if(!empty($_POST['front']) && !empty($_POST['back'])){
     $get_cards->bindParam(':front', $_POST['front']);
     $get_cards->bindParam(':back', $_POST['back']);
     $get_cards->execute();
-    header('Location: http://localhost/flashcards/createCards.php');
+    header('Location: http://localhost:8888/test/flashcards/createCards.php');
 }else{
-    header('Location: http://localhost/flashcards/createCards.php');
+    header('Location: http://localhost:8888/test/flashcards/createCards.php');
 }
 
 ?>
