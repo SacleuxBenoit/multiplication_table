8be9584765e434a65a7e8a9829bd134c87e49a7f
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-04-07 18:20:40 +0200

    rename the database :arrow_right: connection_database.php

--- database/connection_database.php
+++ database/connection_database.php
@@ -1,7 +1,7 @@
 <?php
         try
         {
-            $bdd = new PDO('mysql:host=localhost;dbname=flashcards;charset=utf8', $_SESSION['user'], $_SESSION['pass']);
+            $bdd = new PDO('mysql:host=localhost;dbname=multiplication_tables;charset=utf8', $_SESSION['user'], $_SESSION['pass']);
             $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         catch(Exception $e)
