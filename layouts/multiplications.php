a6678f2110c4b62076717710277ba2b172fad0d4
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-04-08 18:09:18 +0200

    delete the content in index.php due to the merge + add the good path for every links :arrow_right: multiplications.php

--- layouts/multiplications.php
+++ layouts/multiplications.php
@@ -1,20 +1,20 @@
 <?php
     session_start();
-    include('./pass.php');
-    include('database/connection_database.php');
+    include('../pass.php');
+    include('../database/connection_database.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Document</title>
 </head>
 <body>
 
     <?php
-        include('components/header.php');
+        include('../components/header.php');
         $get_cards = $bdd->query('SELECT front,back FROM addcards ORDER BY RAND() LIMIT 1');
         $get_cards->execute();
         while($display_cards = $get_cards->fetch()){
@@ -29,6 +29,6 @@
 
 <button id="refresh" onclick="document.location.reload(false)" class="buttonNext">Next</button>
 
-<script src="./js/script.js"></script>
+<script src="../js/script.js"></script>
 </body>
 </html>
