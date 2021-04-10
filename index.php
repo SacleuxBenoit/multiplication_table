a85ca42da9282fd72446396d5844653d0b8df623
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-04-10 20:50:19 +0200

    create a form + dropdown for the tables :arrow_right: index.php

--- index.php
+++ index.php
@@ -24,5 +24,24 @@
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ipsa ullam possimus iusto vero accusamus quia in ea quisquam recusandae dolore cumque aliquid fugit sequi, dolorem facere autem. Voluptatibus, cum.
     </div>
 
+    <div id="chooseTable">
+        <form action="#" method="post">
+
+            <select name="dropdownTable" id="dropdownTable">
+                <option value="1">Table 1</option>
+                <option value="2">Table 2</option>
+                <option value="3">Table 3</option>
+                <option value="4">Table 4</option>
+                <option value="5">Table 5</option>
+                <option value="6">Table 6</option>
+                <option value="7">Table 7</option>
+                <option value="8">Table 8</option>
+                <option value="9">Table 9</option>
+                <option value="10">Table 10</option>
+            </select> 
+
+            <input type="submit" value="Envoyer">
+        </form>
+    </div>
 </body>
 </html>
