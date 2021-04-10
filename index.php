7f258aa26ab0bbb20b345d7041b0cceb6406ed35
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-04-10 20:45:58 +0200

    create a div with lorem ipsum in it :arrow_right: index.php

--- index.php
+++ index.php
@@ -17,5 +17,12 @@
         include('./components/header.php');
     ?>
     
+    <div id="indexDesc">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis, consequatur. Ut iste tempore quam laboriosam explicabo magnam consequuntur. Officia sint optio voluptates itaque eveniet neque reiciendis sapiente aliquid perferendis tempore.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, dolor atque consectetur possimus iure ratione? Est deleniti, reiciendis modi quos molestias aliquam perspiciatis accusamus sed ratione sunt eos exercitationem. Alias?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quae optio sapiente, ipsam animi dolor ab quisquam. Impedit sit, sint magni optio voluptatum incidunt dignissimos recusandae culpa atque, deleniti non.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ipsa ullam possimus iusto vero accusamus quia in ea quisquam recusandae dolore cumque aliquid fugit sequi, dolorem facere autem. Voluptatibus, cum.
+    </div>
+
 </body>
 </html>
